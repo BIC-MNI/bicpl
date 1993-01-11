6c9995def5df537415d79b92761e4ad92deb7f43
david <david>
1993-01-11 11:20:22 +0000

    Initial revision

---
+++ Prog_utils/arguments.c
@@ -0,0 +1,253 @@
+
+#include  <def_mni.h>
+
+typedef struct
+{
+    int   argc;
+    char  **argv;
+    int   current_arg;
+    int   current_offset_within_arg;
+} arguments_struct;
+
+private  arguments_struct   arguments;
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : arguments_remaining
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : TRUE if some arguments remain to be parsed
+@DESCRIPTION: Checks if all arguments have been parsed.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+private  Boolean  arguments_remaining( void )
+{
+    return( arguments.current_arg < arguments.argc );
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_current_argument_string
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : a pointer to the argument string.
+@DESCRIPTION: Returns a point to an argument string.  This may be one
+            : of the argv[i] parameters, or may point within one of these,
+            : for instance, if the -n part of -nfilename has been parsed.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+private  char  *get_current_argument_string( void )
+{
+    char   *arg;
+
+    arg = arguments.argv[arguments.current_arg];
+    return( &arg[arguments.current_offset_within_arg] );
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : advance_argument
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : 
+@DESCRIPTION: Advances the current arg pointer to the next argument.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+private  void  advance_argument( void )
+{
+    ++arguments.current_arg;
+    arguments.current_offset_within_arg = 0;
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : initialize_argument_processing
+@INPUT      : argc
+            : argv
+@OUTPUT     : 
+@RETURNS    : 
+@DESCRIPTION: Records the arguments from the main program, and initializes
+            : parsing to the first argument.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  void  initialize_argument_processing(
+    int     argc,
+    char    *argv[] )
+{
+    arguments.argc = argc;
+    arguments.argv = argv;
+    arguments.current_arg = 1;
+    arguments.current_offset_within_arg = 0;
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_int_argument
+@INPUT      : default_value
+@OUTPUT     : value
+@RETURNS    : TRUE if next argument exists and is an integer
+@DESCRIPTION: Checks if arguments remain, and if the next one is an integer,
+            : stores it in "value", and advances the current pointer to the
+            : next argument.  Otherwise, value is assigned the default_value.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  Boolean  get_int_argument(
+    int   default_value,
+    int   *value )
+{
+    Boolean   found;
+
+    if( arguments_remaining() &&
+        sscanf( get_current_argument_string(), "%d", value ) == 1 )
+    {
+        advance_argument();
+        found = TRUE;
+    }
+    else
+    {
+        *value = default_value;
+        found = FALSE;
+    }
+
+    return( found );
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_real_argument
+@INPUT      : default_value
+@OUTPUT     : value
+@RETURNS    : TRUE if next argument exists and is a real
+@DESCRIPTION: Checks if arguments remain, and if the next one is a real,
+            : stores it in "value", and advances the current pointer to the
+            : next argument.  Otherwise, value is assigned the default_value.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  Boolean  get_real_argument(
+    Real   default_value,
+    Real   *value )
+{
+    Boolean   found;
+
+    if( arguments_remaining() &&
+        (real_is_double() &&
+         sscanf( get_current_argument_string(), "%lf", (double *) value ) ==1 ||
+         !real_is_double() &&
+         sscanf( get_current_argument_string(), "%f", (float *) value ) == 1
+        ) )
+    {
+        advance_argument();
+        found = TRUE;
+    }
+    else
+    {
+        *value = default_value;
+        found = FALSE;
+    }
+
+    return( found );
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_string_argument
+@INPUT      : default_value
+@OUTPUT     : value
+@RETURNS    : TRUE if next argument exists
+@DESCRIPTION: Checks if arguments remain, stores the pointer to the next one
+            : in "value", and advances the current pointer to the
+            : next argument.  Otherwise, value is assigned the default_value.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  Boolean  get_string_argument(
+    char   default_value[],
+    char   *value[] )
+{
+    Boolean   found;
+
+    if( arguments_remaining() )
+    {
+        *value = get_current_argument_string();
+        advance_argument();
+        found = TRUE;
+    }
+    else
+    {
+        *value = default_value;
+        found = FALSE;
+    }
+
+    return( found );
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_prefix_argument
+@INPUT      : prefix          - string to look for, e.g., "-n"
+@OUTPUT     : 
+@RETURNS    : TRUE if next argument begins with the prefix
+@DESCRIPTION: Checks if arguments remain, and if the next one starts with
+            : the prefix, advances the current pointer to just after the
+            : prefix, which may be within the same argument.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  Boolean  get_prefix_argument(
+    char  prefix[] )
+{
+    char      *next_str;
+    Boolean   found;
+
+    if( arguments_remaining() &&
+        strlen(get_current_argument_string()) >= strlen(prefix) &&
+        strncmp( get_current_argument_string(), prefix, strlen(prefix) ) == 0 )
+    {
+        arguments.current_offset_within_arg += strlen(prefix);
+
+        next_str = get_current_argument_string();
+        while( *next_str == ' ' || *next_str == '\t' )
+            ++next_str;
+
+        if( *next_str == (char) 0 )
+            advance_argument();
+
+        found = TRUE;
+    }
+    else
+    {
+        found = FALSE;
+    }
+
+    return( found );
+}
---
+++ Prog_utils/random.c
@@ -0,0 +1,122 @@
+
+#include  <def_mni.h>
+#include  <sys/time.h>
+
+#define  MAX_RAND  2147483648.0
+
+private  Boolean  initialized = FALSE;
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : set_random_seed
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : 
+@DESCRIPTION: Sets the random seed for the generator
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  void  set_random_seed( int seed )
+{
+#ifndef sgi
+    int  srandom();
+#endif
+    (void) srandom( seed );
+    initialized = TRUE;
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : check_initialized
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : 
+@DESCRIPTION: Ensures that the random seed has been set.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+private  void  check_initialized( void )
+{
+    struct   timeval   t;
+    int                seed;
+
+    if( !initialized )
+    {
+        (void) gettimeofday( &t, (struct timezone *) 0 );
+
+        seed = (int) t.tv_usec;
+
+        set_random_seed( seed );
+    }
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_random
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : int
+@DESCRIPTION: Gets a random number from 0 to 2^30
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+private  int  get_random( void )
+{
+    check_initialized();
+
+#ifdef sgi
+    return( random() );
+#else
+    {
+        long random();
+
+        return( (int) random() );
+    }
+#endif
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_random_int
+@INPUT      : n
+@OUTPUT     : 
+@RETURNS    : int
+@DESCRIPTION: Returns a random number from 0 to n-1.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  int  get_random_int( int n )
+{
+    return( get_random() % n );
+}
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : get_random_0_to_1
+@INPUT      : 
+@OUTPUT     : 
+@RETURNS    : random number
+@DESCRIPTION: Returns a random number greater than or equal to 0 and and less
+            : than 1.
+@METHOD     : 
+@GLOBALS    : 
+@CALLS      : 
+@CREATED    :                      David MacDonald
+@MODIFIED   : 
+---------------------------------------------------------------------------- */
+
+public  Real  get_random_0_to_1( void )
+{
+    return( (Real) get_random() / MAX_RAND );
+}
