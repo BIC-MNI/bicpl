9b0e6606bf3e395a1acbc3f003d9ff3a3da2da47
david <david>
1993-03-02 15:49:51 +0000

    *** empty log message ***

--- Objects/objects.c
+++ Objects/objects.c
@@ -26,25 +26,89 @@ public  void  set_object_visibility(
     object->visibility = visibility;
 }
 
-public  void  *get_object_pointer(
+public  lines_struct  *get_lines_ptr(
     object_struct  *object )
 {
-    void   *ptr;
-
-    switch( object->object_type )
+    if( object->object_type != LINES )
     {
-    case  LINES:       ptr = (void *) &object->specific.lines;         break;
-    case  MARKER:      ptr = (void *) &object->specific.marker;        break;
-    case  PIXELS:      ptr = (void *) &object->specific.pixels;        break;
-    case  POLYGONS:    ptr = (void *) &object->specific.polygons;      break;
-    case  QUADMESH:    ptr = (void *) &object->specific.quadmesh;      break;
-    case  TEXT:        ptr = (void *) &object->specific.text;          break;
+        HANDLE_INTERNAL_ERROR( "get_lines_ptr" );
+        return( (lines_struct *) NULL );
     }
-
-    return( ptr );
+    else
+        return( &object->specific.lines );
 }
 
-/* --------------------- object functions ------------------------ */
+public  marker_struct  *get_marker_ptr(
+    object_struct  *object )
+{
+    if( object->object_type != MARKER )
+    {
+        HANDLE_INTERNAL_ERROR( "get_marker_ptr" );
+        return( (marker_struct *) NULL );
+    }
+    else
+        return( &object->specific.marker );
+}
+
+public  model_struct  *get_model_ptr(
+    object_struct  *object )
+{
+    if( object->object_type != MODEL )
+    {
+        HANDLE_INTERNAL_ERROR( "get_model_ptr" );
+        return( (model_struct *) NULL );
+    }
+    else
+        return( &object->specific.model );
+}
+
+public  pixels_struct  *get_pixels_ptr(
+    object_struct  *object )
+{
+    if( object->object_type != PIXELS )
+    {
+        HANDLE_INTERNAL_ERROR( "get_pixels_ptr" );
+        return( (pixels_struct *) NULL );
+    }
+    else
+        return( &object->specific.pixels );
+}
+
+public  polygons_struct  *get_polygons_ptr(
+    object_struct  *object )
+{
+    if( object->object_type != POLYGONS )
+    {
+        HANDLE_INTERNAL_ERROR( "get_polygons_ptr" );
+        return( (polygons_struct *) NULL );
+    }
+    else
+        return( &object->specific.polygons );
+}
+
+public  quadmesh_struct  *get_quadmesh_ptr(
+    object_struct  *object )
+{
+    if( object->object_type != QUADMESH )
+    {
+        HANDLE_INTERNAL_ERROR( "get_quadmesh_ptr" );
+        return( (quadmesh_struct *) NULL );
+    }
+    else
+        return( &object->specific.quadmesh );
+}
+
+public  text_struct  *get_text_ptr(
+    object_struct  *object )
+{
+    if( object->object_type != TEXT )
+    {
+        HANDLE_INTERNAL_ERROR( "get_text_ptr" );
+        return( (text_struct *) NULL );
+    }
+    else
+        return( &object->specific.text );
+}
 
 /* --------------------- draw functions ------------------------ */
 
@@ -52,42 +116,48 @@ private  void  draw_lines(
     window_struct   *window,
     object_struct   *object )
 {
-    G_draw_lines( window, (lines_struct *) get_object_pointer(object) );
+    G_draw_lines( window, get_lines_ptr(object) );
 }
 
 private  void  draw_marker(
     window_struct   *window,
     object_struct   *object )
 {
-    G_draw_marker( window, (marker_struct *) get_object_pointer(object) );
+    G_draw_marker( window, get_marker_ptr(object) );
+}
+
+private  void  draw_model(
+    window_struct   *window,
+    object_struct   *object )                    /* ARGSUSED */
+{
 }
 
 private  void  draw_pixels(
     window_struct   *window,
     object_struct   *object )
 {
-    G_draw_pixels( window, (pixels_struct *) get_object_pointer(object) );
+    G_draw_pixels( window, get_pixels_ptr(object) );
 }
 
 private  void  draw_polygons(
     window_struct   *window,
     object_struct   *object )
 {
-    G_draw_polygons( window, (polygons_struct *) get_object_pointer(object) );
+    G_draw_polygons( window, get_polygons_ptr(object) );
 }
 
 private  void  draw_quadmesh(
     window_struct   *window,
     object_struct   *object )
 {
-    G_draw_quadmesh( window, (quadmesh_struct *) get_object_pointer(object) );
+    G_draw_quadmesh( window, get_quadmesh_ptr(object) );
 }
 
 private  void  draw_text(
     window_struct   *window,
     object_struct   *object )
 {
-    G_draw_text( window, (text_struct *) get_object_pointer(object) );
+    G_draw_text( window, get_text_ptr(object) );
 }
 
 /* --------------------- delete functions ------------------------ */
@@ -95,7 +165,7 @@ private  void  draw_text(
 private  void  delete_lines_object(
     object_struct   *object )
 {
-    delete_lines( (lines_struct *) get_object_pointer(object) );
+    delete_lines( get_lines_ptr(object) );
 }
 
 private  void  delete_marker_object(
@@ -103,16 +173,28 @@ private  void  delete_marker_object(
 {
 }
 
+private  void  delete_model_object(
+    object_struct   *object )
+{
+    int            i;
+    model_struct   *model;
+
+    model = get_model_ptr( object );
+
+    for_less( i, 0, model->n_objects )
+        delete_object( model->objects[i] );
+}
+
 private  void  delete_pixels_object(
     object_struct   *object )
 {
-    delete_pixels( (pixels_struct *) get_object_pointer(object) );
+    delete_pixels( get_pixels_ptr(object) );
 }
 
 private  void  delete_polygons_object(
     object_struct   *object )
 {
-    delete_polygons( (polygons_struct *) get_object_pointer(object) );
+    delete_polygons( get_polygons_ptr(object) );
 }
 
 private  void  delete_quadmesh_object(
@@ -144,6 +226,10 @@ static  object_functions_list   object_functions[N_OBJECT_TYPES] =
         draw_marker,
         delete_marker_object
     },                              /* MARKER */
+    {
+        draw_model,
+        delete_model_object
+    },                              /* MODEL */
     {
         draw_pixels,
         delete_pixels_object
