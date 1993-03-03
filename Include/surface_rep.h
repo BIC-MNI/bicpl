6fcfe3c4a501764685bbd11b3d0b6d8f21da1df9
david <david>
1993-03-03 17:21:27 +0000

    Initial revision

---
+++ Include/surface_rep.h
@@ -0,0 +1,33 @@
+#ifndef  DEF_SURFACE_REP
+#define  DEF_SURFACE_REP
+
+#include  <def_mni.h>
+
+typedef  enum  { SUPERQUADRIC, SPLINE,
+                 N_SURFACE_REPRESENTATIONS }
+               Surface_representations;
+
+typedef struct surface_rep_struct
+{
+    Surface_representations  representation;
+    int                      n_descriptors;
+
+    int                       (*get_num_parameters)( double [] );
+    void                      (*get_default_descriptors)( double [] );
+    Boolean                   (*are_parameters_valid)( double [], double []);
+    void                      (*get_parameter_influence)( int, double [],
+                                                          double *, double *, 
+                                                          double *, double * );
+    void                      (*convert_from_representation)( 
+                                   struct surface_rep_struct *,
+                                   double [], double [], double [], double [] );
+    void                      (*evaluate_surface_at_uv)( double, double,
+                                   double [], double [],
+                                   double *, double *, double *,
+                                   double *, double *, double *,
+                                   double *, double *, double *,
+                                   double *, double *, double *,
+                                   double *, double *, double * );
+} surface_rep_struct;
+
+#endif
