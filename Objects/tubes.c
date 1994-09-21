e5a83af468d246eb4d0a7a1166c9d8cbdb757a36
david <david>
1994-09-21 15:09:16 +0000

    Initial revision

---
+++ Objects/tubes.c
@@ -0,0 +1,22 @@
+
+#include  <mni.h>
+
+public  void  create_tube(
+    int               n_points,
+    Point             points[],
+    int               n_around,
+    Real              radius,
+    Colour            colour,
+    quadmesh_struct   *quadmesh )
+{
+    Surfprop    spr;
+
+    get_default_surfprop( &spr );
+
+    initialize_quadmesh( quadmesh, colour, &spr, n_points, n_around );
+    quadmesh->m_closed = FALSE;
+    quadmesh->n_closed = TRUE;
+
+    generate_tube( n_points, points, n_around, radius,
+                   quadmesh->points, quadmesh->normals );
+}
