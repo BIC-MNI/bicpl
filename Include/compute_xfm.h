c2e09ae36b2fb6e1337314cde10772321831b369
david <david>
1994-11-04 13:11:34 +0000

    Initial revision

---
+++ Include/compute_xfm.h
@@ -0,0 +1,34 @@
+#ifndef  _COMPUTE_XFM_H
+#define  _COMPUTE_XFM_H
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : compute_xfm.h
+@DESCRIPTION: Header file for compute_transform_from_tags
+@GLOBALS    :
+@CALLS      :
+@CREATED    : August 30, 1993 (Peter Neelin)
+@MODIFIED   : $Log: compute_xfm.h,v $
+@MODIFIED   : Revision 1.1  1994-11-04 13:11:36  david
+@MODIFIED   : Initial revision
+@MODIFIED   :
+ * Revision 1.2  93/09/08  15:47:44  neelin
+ * Added 9 and 10 parameter linear fitting.
+ * 
+ * Revision 1.1  93/09/01  15:25:37  neelin
+ * Initial revision
+ * 
+ * Revision 1.1  93/09/01  13:22:24  neelin
+ * Initial revision
+ * 
+---------------------------------------------------------------------------- */
+
+/* Transformation types */
+typedef enum {
+   TRANS_LSQ6, TRANS_LSQ7, TRANS_LSQ9, TRANS_LSQ10, TRANS_LSQ12, TRANS_TPS
+} Trans_type;
+
+/* Minimum number of points needed */
+#define MIN_POINTS_LINEAR 4
+#define MIN_POINTS_TPS    5
+
+#endif
