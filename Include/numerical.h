1f603615ba1242d9916e97894e5efafb48ba2993
david <david>
1994-12-17 20:35:04 +0000

    Initial revision

---
+++ Include/numerical.h
@@ -0,0 +1,24 @@
+#ifndef  _NUMERICAL_H
+#define  _NUMERICAL_H
+
+#include  <volume_io.h>
+#include  <objects.h>
+#include  <amoeba.h>
+#include  <histogram.h>
+#include  <interval.h>
+#include  <minimization.h>
+#include  <splines.h>
+
+#ifndef  public
+#define       public   extern
+#define       public_was_defined_here
+#endif
+
+#include  <numeric_prototypes.h>
+
+#ifdef  public_was_defined_here
+#undef       public
+#undef       public_was_defined_here
+#endif
+
+#endif
