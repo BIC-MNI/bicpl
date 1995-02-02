d8aa785971a27df5108821f2264ff2c6d0925539
david <david>
1995-02-02 13:27:25 +0000

    *** empty log message ***

--- Numerical/gaussian.c
+++ Numerical/gaussian.c
@@ -1,19 +1,23 @@
-#include  <internal_volume_io.h>
-#include  <bicpl.h>
+#ifndef lint
+static char rcsid[] = "$Header";
+#endif
 
-public  BOOLEAN  solve_linear_system(
+#include  <internal_volume_io.h>
+#include  <numerical.h>
+
+private  BOOLEAN  scaled_maximal_pivoting_gaussian_elimination_float(
     int   n,
-    Real  **coefs,
-    Real  values[],
-    Real  solution[] )
+    float **coefs,
+    int   n_values,
+    float **values )
 {
-    int       i, j, k, p;
-    Real      **a, *s, val, best_val, m, sum, *tmp;
+    int       i, j, k, p, v;
+    float     **a, *s, tmp_real, val, best_val, m, *tmp, scale_factor;
     BOOLEAN   success;
 
     ALLOC( a, n );
     for_less( i, 0, n )
-        ALLOC( a[i], n+1 );
+        ALLOC( a[i], n );
 
     ALLOC( s, n );
 
@@ -21,7 +25,6 @@ public  BOOLEAN  solve_linear_system(
     {
         for_less( j, 0, n )
             a[i][j] = coefs[i][j];
-        a[i][n] = values[i];
     }
 
     for_less( i, 0, n )
@@ -41,7 +44,7 @@ public  BOOLEAN  solve_linear_system(
         p = i;
         best_val = a[i][i] / s[i];
         best_val = ABS( best_val );
-        for_less( j, i, n )
+        for_less( j, i+1, n )
         {
             val = a[j][i] / s[j];
             val = ABS( val );
@@ -63,25 +66,38 @@ public  BOOLEAN  solve_linear_system(
             tmp = a[i];
             a[i] = a[p];
             a[p] = tmp;
+            tmp_real = s[i];
+            s[i] = s[p];
+            s[p] = tmp_real;
         }
 
         for_less( j, i+1, n )
         {
             m = a[j][i] / a[i][i];
-            for_less( k, i, n+1 )
+            for_less( k, i+1, n )
                 a[j][k] -= m * a[i][k];
+            for_less( v, 0, n_values )
+                values[v][j] -= m * values[v][i];
         }
     }
 
     if( success && a[n-1][n-1] == 0.0 )
         success = FALSE;
 
-    for( i = n-1;  i >= 0;  --i )
+    if( success )
     {
-        sum = 0.0;
-        for_less( j, i+1, n )
-            sum += a[i][j] * solution[j];
-        solution[i] = (a[i][n] - sum) / a[i][i];
+        for( i = n-1;  i >= 0;  --i )
+        {
+            for_less( j, i+1, n )
+            {
+                scale_factor = a[i][j];
+                for_less( v, 0, n_values )
+                    values[v][i] -= scale_factor * values[v][j];
+            }
+
+            for_less( v, 0, n_values )
+                values[v][i] /= a[i][i];
+        }
     }
 
     for_less( i, 0, n )
@@ -89,6 +105,56 @@ public  BOOLEAN  solve_linear_system(
     FREE( a );
 
     FREE( s );
+
+    return( success );
+}
+
+public  BOOLEAN  solve_linear_system_float(
+    int    n,
+    float  **coefs,
+    float  values[],
+    float  solution[] )
+{
+    int       i;
+
+    for_less( i, 0, n )
+        solution[i] = values[i];
+
+    return( scaled_maximal_pivoting_gaussian_elimination_float( n, coefs, 1,
+                                                                &solution ) );
+}
+
+public  BOOLEAN  invert_square_matrix_float(
+    int    n,
+    float  **matrix,
+    float  **inverse )
+{
+    float     tmp;
+    BOOLEAN   success;
+    int       i, j;
+
+    for_less( i, 0, n )
+    {
+        for_less( j, 0, n )
+            inverse[i][j] = 0.0;
+        inverse[i][i] = 1.0;
+    }
+
+    success = scaled_maximal_pivoting_gaussian_elimination_float( n, matrix,
+                                                                  n, inverse );
+
+    if( success )
+    {
+        for_less( i, 0, n-1 )
+        {
+            for_less( j, i+1, n )
+            {
+                tmp = inverse[i][j];
+                inverse[i][j] = inverse[j][i];
+                inverse[j][i] = tmp;
+            }
+        }
+    }
 
     return( success );
 }
