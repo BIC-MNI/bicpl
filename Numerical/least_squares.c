d8786d6939efa71550b71b970c6346c4a908b3c2
david <david>
1995-02-27 17:16:37 +0000

    Initial revision

---
+++ Numerical/least_squares.c
@@ -0,0 +1,58 @@
+
+#include  <internal_volume_io.h>
+#include  <geom.h>
+
+#ifndef lint
+static char rcsid[] = "$Header: /private-cvsroot/libraries/bicpl/Numerical/least_squares.c,v 1.1 1995-02-27 17:16:37 david Exp $";
+#endif
+
+private  void  least_squares(
+    int     n_points,
+    int     n_dims,
+    Real    **points,
+    Real    values[],
+    Real    parameters[] )
+{
+    int      pt, i, j;
+    Real     **coefs, *constants, *p;
+
+    ALLOC2D( coefs, n_dims+1, n_dims+1 );
+    ALLOC( constants, n_dims+1 );
+    ALLOC( p, n_dims+1 );
+
+    for_less( i, 0, n_dims+1 )
+    {
+        for_less( j, 0, n_dims+1 )
+            coefs[i][j] = 0.0;
+
+        constants[i] = 0.0;
+    }
+
+    for_less( pt, 0, n_points )
+    {
+        p[0] = 1.0;
+        for_less( i, 0, n_dims )
+            p[i+1] = points[pt][i];
+
+        for_less( i, 0, n_dims+1 )
+        {
+            for_less( j, 0, n_dims+1 )
+                coefs[i][j] += p[i] * p[j];
+
+            constants[i] += p[i] * values[pt];
+        }
+    }
+
+    for_less( i, 0, n_dims+1 )
+    {
+        for_less( j, i+1, n_dims+1 )
+            coefs[j][i] = coefs[i][j];
+    }
+
+    if( !solve_linear_system( n_dims+1, coefs, constants, parameters ) )
+    {
+        print( "Could not solve least squares, non-invertible matrix.\n" );
+        for_less( i, 0, n_dims+1 )
+            parameters[i] = 0.0;
+    }
+}
