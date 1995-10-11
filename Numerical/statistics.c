0389704300253aeba7b8b25c5a8a0e5844a3356e
david <david>
1995-10-11 17:01:13 +0000

    *** empty log message ***

--- Numerical/statistics.c
+++ Numerical/statistics.c
@@ -17,8 +17,10 @@
 #include  <data_structures.h>
 
 #ifndef lint
-static char rcsid[] = "$Header: /private-cvsroot/libraries/bicpl/Numerical/statistics.c,v 1.5 1995-07-31 13:45:25 david Exp $";
+static char rcsid[] = "$Header: /private-cvsroot/libraries/bicpl/Numerical/statistics.c,v 1.6 1995-10-11 17:01:13 david Exp $";
 #endif
+
+#define  DEFAULT_N_MEDIAN_BOXES    10000
 
 /* ----------------------------- MNI Header -----------------------------------
 @NAME       : compute_statistics
@@ -47,64 +49,266 @@ public  void  compute_statistics(
     Real     *std_dev,
     Real     *median )
 {
-    int                              i;
-    char                             ignored;
-    Real                             x, sum_x, sum_xx, variance;
-    PRIORITY_QUEUE_STRUCT( char )    heap;
-
-    *min_value = 0.0;
-    *max_value = 0.0;
-    sum_x = 0.0;
-    sum_xx = 0.0;
+    int                   i;
+    Real                  x, min_median, max_median;
+    statistics_struct     stats;
+    BOOLEAN               median_is_exact, done;
 
     if( median != NULL )
     {
-        INITIALIZE_PRIORITY_QUEUE( heap );
-    }
+        min_median = 0.0;
+        max_median = 0.0;
 
-    for_less( i, 0, n )
-    {
-        x = samples[i];
-        if( i == 0 )
+        for_less( i, 0, n )
         {
-            *min_value = x;
-            *max_value = x;
-        }
-        else if( x < *min_value )
-            *min_value = x;
-        else if( x > *max_value )
-            *max_value = x;
-
-        sum_x += x;
-        sum_xx += x * x;
-
-        if( median != NULL )
-        {
-            INSERT_IN_PRIORITY_QUEUE( heap, (char) 0, x );
+            x = samples[i];
+            if( i == 0 )
+            {
+                min_median = x;
+                max_median = x;
+            }
+            else if( x < min_median )
+                min_median = x;
+            else if( x > max_median )
+                max_median = x;
         }
     }
-
-    if( median != NULL )
-    {
-        for_less( i, 0, (n+1)/2 )
-            REMOVE_FROM_PRIORITY_QUEUE( heap, ignored, *median );
-
-        DELETE_PRIORITY_QUEUE( heap );
-    }
-
-#ifdef lint
-    if( ignored != (char) 0 ) { ignored = 0; }
-#endif
-
-    if( n == 0 )
-        *mean_value = 0.0;
     else
-        *mean_value = sum_x / (Real) n;
+    {
+        min_median = 0.0;
+        max_median = -1.0;
+    }
+
+    initialize_statistics( &stats, min_median, max_median );
+
+    done = FALSE;
+
+    while( !done )
+    {
+        for_less( i, 0, n )
+            add_sample_to_statistics( &stats, samples[i] );
+
+        get_statistics( &stats, NULL, mean_value, median, &median_is_exact,
+                        min_value, max_value, std_dev );
+
+        if( median != NULL && !median_is_exact )
+            restart_statistics_with_narrower_median_range( &stats );
+        else
+            done = TRUE;
+    }
+
+    terminate_statistics( &stats );
+}
+
+public  void  initialize_statistics(
+    statistics_struct  *stats,
+    Real               median_lower_bound,
+    Real               median_upper_bound )
+{
+    int    i;
+
+    stats->n_samples = 0;
+    stats->sum_x = 0.0;
+    stats->sum_xx = 0.0;
+
+    stats->min_median_range = median_lower_bound;
+    stats->max_median_range = median_upper_bound;
+
+    if( median_lower_bound < median_upper_bound )
+    {
+        stats->n_median_boxes = DEFAULT_N_MEDIAN_BOXES;
+        ALLOC( stats->median_box_counts, stats->n_median_boxes );
+        ALLOC( stats->median_box_values, stats->n_median_boxes );
+
+        for_less( i, 0, stats->n_median_boxes )
+            stats->median_box_counts[i] = 0;
+
+        stats->n_below_median_range = 0;
+        stats->n_above_median_range = 0;
+    }
+}
+
+public  void  add_sample_to_statistics(
+    statistics_struct  *stats,
+    Real               sample )
+{
+    int         median_box;
+
+    if( stats->n_samples == 0 )
+    {
+        stats->min_value = sample;
+        stats->max_value = sample;
+    }
+    else if( sample < stats->min_value )
+        stats->min_value = sample;
+    else if( sample > stats->max_value )
+        stats->max_value = sample;
+
+    stats->n_samples += 1;
+    stats->sum_x += sample;
+    stats->sum_xx += sample * sample;
+
+    if( stats->min_median_range < stats->max_median_range )
+    {
+        if( sample < stats->min_median_range )
+            ++stats->n_below_median_range;
+        else if( sample >= stats->max_median_range )
+            ++stats->n_above_median_range;
+        else
+        {
+            median_box = stats->n_median_boxes *
+                         (sample - stats->min_median_range)/
+                         (stats->max_median_range - stats->min_median_range);
+
+            ++stats->median_box_counts[median_box];
+            if( stats->median_box_counts[median_box] == 1 )
+                stats->median_box_values[median_box] = sample;
+        }
+    }
+}
+
+private  void  get_median(
+    statistics_struct  *stats,
+    Real               *min_range,
+    Real               *max_range )
+{
+    int   box_index, median_index;
+
+    if( stats->min_median_range >= stats->max_median_range )
+    {
+        *min_range = -1.0e30;
+        *max_range = 1.0e30;
+    }
+
+    median_index = stats->n_samples / 2;
+
+    if( median_index < stats->n_below_median_range )
+    {
+        *min_range = stats->min_value;
+        *max_range = stats->min_median_range;
+        return;
+    }
+
+    median_index -= stats->n_below_median_range;
+
+    box_index = 0;
+    while( box_index < stats->n_median_boxes &&
+           median_index >= stats->median_box_counts[box_index] )
+    {
+        median_index -= stats->median_box_counts[box_index];
+        ++box_index;
+    }
+
+    if( box_index == stats->n_median_boxes )
+    {
+        *min_range = stats->max_median_range;
+        *max_range = stats->max_value;
+    }
+    else
+    {
+        if( stats->median_box_counts[box_index] == 1 )
+        {
+            *min_range = stats->median_box_values[box_index];
+            *max_range = stats->median_box_values[box_index];
+        }
+        else
+        {
+            *min_range = stats->min_median_range +
+                         (stats->max_median_range - stats->min_median_range) *
+                          (Real) box_index / (Real) stats->n_median_boxes;
+            *max_range = stats->min_median_range +
+                         (stats->max_median_range - stats->min_median_range) *
+                          (Real) (box_index+1) / (Real) stats->n_median_boxes;
+        }
+    }
+}
+
+public  void  restart_statistics_with_narrower_median_range(
+    statistics_struct  *stats )
+{
+    Real   min_median_range, max_median_range;
+
+    get_median( stats, &min_median_range, &max_median_range );
+
+    if( min_median_range == max_median_range )
+    {
+        min_median_range = stats->min_median_range;
+        max_median_range = stats->max_median_range;
+        print_error( "Median range already narrow enough.\n" );
+    }
+
+    terminate_statistics( stats );
+
+    initialize_statistics( stats, min_median_range, max_median_range );
+}
+
+public  void  get_statistics(
+    statistics_struct  *stats,
+    int                *n_samples,
+    Real               *mean,
+    Real               *median,
+    BOOLEAN            *median_is_exact,
+    Real               *min_value,
+    Real               *max_value,
+    Real               *std_deviation )
+{
+    BOOLEAN  exact;
+    int      n;
+    Real     sum_x, sum_xx, min_median_range, max_median_range, variance;
+
+    if( n_samples != NULL )
+        *n_samples = stats->n_samples;
+
+    if( stats->n_samples <= 0 )
+    {
+        if( median_is_exact != NULL )
+            *median_is_exact = TRUE;
+        return;
+    }
+
+    if( median != NULL )
+    {
+        get_median( stats, &min_median_range, &max_median_range );
+
+        exact = (min_median_range == max_median_range);
+
+        if( median_is_exact != NULL )
+            *median_is_exact = exact;
+
+        if( exact )
+            *median = min_median_range;
+        else
+            *median = (min_median_range + max_median_range) / 2.0;
+    }
+
+    if( min_value != NULL )
+        *min_value = stats->min_value;
+
+    if( max_value != NULL )
+        *max_value = stats->max_value;
+
+    n = stats->n_samples;
+    sum_x = stats->sum_x;
+    sum_xx = stats->sum_xx;
+
+    if( mean != NULL )
+        *mean = sum_x / (Real) n;
 
     if( n == 1 )
         variance = 0.0;
     else
         variance = (sum_xx - sum_x * sum_x / (Real) n) / (Real) (n - 1);
 
-    *std_dev = sqrt( variance );
+    if( std_deviation != NULL )
+        *std_deviation = sqrt( variance );
+}
+
+public  void  terminate_statistics(
+    statistics_struct  *stats )
+{
+    if( stats->min_median_range < stats->max_median_range )
+    {
+        FREE( stats->median_box_counts );
+        FREE( stats->median_box_values );
+    }
 }
