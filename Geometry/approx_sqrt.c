b12fc7d6ac7182956ab18cf631cc725c08e5e452
david <david>
1996-07-04 15:36:46 +0000

    Initial revision

---
+++ Geometry/approx_sqrt.c
@@ -0,0 +1,67 @@
+#include  <internal_volume_io.h>
+#include  <special_geometry.h>
+
+#undef   USE_FAST_APPROX
+#define  USE_FAST_APPROX
+
+#ifdef  USE_FAST_APPROX
+#define  FAST_SQRT  fast_approx_sqrt
+#else
+#define  FAST_SQRT  sqrt
+#endif
+
+#define  MAX_SQRT_ARG              1000.0
+#define  N_SQRT_PRECOMPUTE         30000
+
+public  double  fast_approx_sqrt(
+    double  y )
+{
+    Real     x;
+    static   BOOLEAN  first = TRUE;
+    static   struct {  double  scale; double trans; }
+                                 precomp_sqrt[N_SQRT_PRECOMPUTE], *lookup;
+
+    if( y >= MAX_SQRT_ARG )
+    {
+        return( sqrt( y ) );
+    }
+
+    if( first )
+    {
+        int    i;
+        Real   next_sqrt, this_sqrt;
+
+        first = FALSE;
+
+        next_sqrt = 0.0;
+        for_less( i, 0, N_SQRT_PRECOMPUTE )
+        {
+            this_sqrt = next_sqrt;
+            next_sqrt = sqrt( (Real) (i+1) / (Real) N_SQRT_PRECOMPUTE *
+                              MAX_SQRT_ARG );
+
+            precomp_sqrt[i].scale = next_sqrt - this_sqrt;
+            precomp_sqrt[i].trans = this_sqrt -
+                                    precomp_sqrt[i].scale * (Real) i;
+        }
+    }
+
+    x = ((Real) N_SQRT_PRECOMPUTE / MAX_SQRT_ARG) * y;
+
+    lookup = &precomp_sqrt[(int) x];
+
+    return( lookup->trans + lookup->scale * x );
+}
+
+public  Real  fast_approx_distance_between_points(
+    Point  *p1,
+    Point  *p2 )
+{
+    Real   dx, dy, dz;
+
+    dx = (Real) Point_x(*p2) - (Real) Point_x(*p1);
+    dy = (Real) Point_y(*p2) - (Real) Point_y(*p1);
+    dz = (Real) Point_z(*p2) - (Real) Point_z(*p1);
+
+    return( FAST_SQRT( dx * dx + dy * dy + dz * dz ) );
+}
