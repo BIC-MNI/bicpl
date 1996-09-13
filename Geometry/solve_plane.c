d6d504d20699b28945940b0217419228acd3d840
david <david>
1996-09-13 17:43:53 +0000

    *** empty log message ***

--- Geometry/solve_plane.c
+++ Geometry/solve_plane.c
@@ -14,7 +14,7 @@
 
 
 #ifndef lint
-static char rcsid[] = "$Header: /private-cvsroot/libraries/bicpl/Geometry/solve_plane.c,v 1.3 1996-09-13 13:47:22 david Exp $";
+static char rcsid[] = "$Header: /private-cvsroot/libraries/bicpl/Geometry/solve_plane.c,v 1.4 1996-09-13 17:43:53 david Exp $";
 #endif
 
 #include  <geom.h>
@@ -31,9 +31,9 @@ public  BOOLEAN  get_interpolation_weights_2d(
     Real   weights[] )
 {
     int   i;
-    Real  av_fact, bv_fact, constant, n;
+    Real  x_fact, y_fact, constant, n;
     Real  aa, ab, ac, bb, bc, dx, dy;
-    Real  acbb, abac, aanbb4, denom, aabc, ab2n;
+    Real  acbb, abac, aabb, denom, aabc, abab;
 
     aa = 0.0;
     ab = 0.0;
@@ -46,30 +46,30 @@ public  BOOLEAN  get_interpolation_weights_2d(
         dx = xs[i] - x;
         dy = ys[i] - y;
         aa += dx * dx;
-        ab += 2.0 * dx * dy;
-        ac += 2.0 * dx;
+        ab += dx * dy;
+        ac += dx;
         bb += dy * dy;
-        bc += 2.0 * dy;
+        bc += dy;
     }
 
     n = (Real) n_points;
-    aanbb4 = 4.0 * aa * n * bb;
+    aabb = aa * bb;
     acbb = ac * bb;
     aabc = aa * bc;
-    ab2n = ab * ab * n;
+    abab = ab * ab;
     abac = ab * ac;
 
-    denom = 2.0 * (aanbb4 - ac * acbb - bc * aabc - ab2n + abac * bc);
+    denom = -aabb * n + ac * acbb + bc * aabc + abab*n - 2.0 * abac * bc;
 
     if( denom == 0.0 )
         return( FALSE );
 
-    bv_fact = -2.0 * (2.0 * aabc - abac) / denom;
-    av_fact = -2.0 * (2.0 * acbb - ab * bc) / denom;
-    constant = (2.0 * aanbb4 - 2.0 * ab2n) / denom - x * av_fact - y * bv_fact;
+    x_fact = (acbb - ab * bc) / denom;
+    y_fact = (aabc - abac) / denom;
+    constant = (abab - aabb) / denom - x * x_fact - y * y_fact;
 
     for_less( i, 0, n_points )
-        weights[i] = constant + bv_fact * ys[i] + av_fact * xs[i];
+        weights[i] = constant + y_fact * ys[i] + x_fact * xs[i];
 
     return( TRUE );
 }
