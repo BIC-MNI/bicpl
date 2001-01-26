b02352ddb1a11de69c664ca3f8e2e4916a938f07
stever <stever>
2001-01-26 22:01:21 +0000

    improved error message

--- Images/rgb_io_none.c
+++ Images/rgb_io_none.c
@@ -6,7 +6,8 @@ public  Status  input_rgb_file(
     STRING          filename,
     pixels_struct   *pixels )
 {
-    print_error( "input_rgb_file(): recompile bicpl with an image library.\n" );
+    print_error( "input_rgb_file(): cannot read image files.\n" );
+    print_error( "Recompile bicpl with an image library, then relink this application.\n" );
 }
 
 
@@ -15,5 +16,6 @@ public  Status  output_rgb_file(
     STRING          filename,
     pixels_struct   *pixels )
 {
-    print_error( "output_rgb_file(): recompile bicpl with an image library.\n" );
+    print_error( "output_rgb_file(): cannot write image files.\n" );
+    print_error( "Recompile bicpl with an image library, then relink this application.\n" );
 }
