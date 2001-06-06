7fab1892f5aece6aaacab21aff98c4d9dd7e1639
stever <stever>
2001-06-06 01:49:56 +0000

    Add optional second argument to bicobj2oogl.

--- tools/bicobj2oogl.c
+++ tools/bicobj2oogl.c
@@ -8,6 +8,7 @@
 #include <bicpl.h>
 
 
+FILE* out = NULL;
 
 
 void polygons_to_off( const polygons_struct* p )
@@ -17,34 +18,36 @@ void polygons_to_off( const polygons_struct* p )
     int n_edges = -1;
     int i,e,f;
 
-    printf( "# A polygon\n" );
-    printf( "OFF\n\n" );
+    fprintf( out, "# A polygon\n" );
+    fprintf( out, "OFF\n\n" );
 
-    printf( "# NVertices NFaces NEdges\n" );
-    printf( "#(Geomview does not check these numbers currently, so we don't bother"
-	    " computing NEdges)\n" );
-    printf( "%d %d %d\n\n", n_vertices, n_faces, n_edges );
+    fprintf( out, "# NVertices NFaces NEdges\n" );
+    fprintf( out, "#(Geomview does not check these numbers currently,"
+	          " so we don't bother computing NEdges)\n" );
+    fprintf( out, "%d %d %d\n\n", n_vertices, n_faces, n_edges );
 
-    printf( "# Coordinates of the %d vertices, one per line.\n", n_vertices );
+    fprintf( out, "# Coordinates of the %d vertices, one per line.\n", n_vertices );
     for( i = 0; i < n_vertices; ++i ) {
-	printf( "%f %f %f\n", 
-		Point_x( p->points[i] ), Point_y( p->points[i] ), Point_z( p->points[i] ) );
+	fprintf( out, "%f %f %f\n", 
+		 Point_x( p->points[i] ), 
+		 Point_y( p->points[i] ), 
+		 Point_z( p->points[i] ) );
     }
-    printf("\n");
+    fprintf( out, "\n");
 
-    printf( "# Vertex indices for each face\n" );
-    printf( "# NVertices for the face, v[0] v[1] ...\n" );
+    fprintf( out, "# Vertex indices for each face\n" );
+    fprintf( out, "# NVertices for the face, v[0] v[1] ...\n" );
     for ( f = 0; f < n_faces; ++f ) {
 	int n_face_edges = GET_OBJECT_SIZE( *p, f );
 
-	printf( "%d ", n_face_edges );
+	fprintf( out, "%d ", n_face_edges );
 	for ( e = 0; e < n_face_edges; ++e ) {
 	    int index = p->indices[POINT_INDEX(p->end_indices, f, e )];
-	    printf("%d ", index );
+	    fprintf( out,"%d ", index );
 	}
-	printf("\n");
+	fprintf( out, "\n");
     }
-    printf("\n");
+    fprintf( out, "\n");
 }
 
 
@@ -60,19 +63,19 @@ void process_file( char* filename )
 	return;
     }
 
-    printf( "LIST\n\n" );
+    fprintf( out, "LIST\n\n" );
 
     for ( i = 0; i < num_objects; ++i ) {
 	Object_types ot = (object_list[i])->object_type;
 
-	printf( "# Object %d of %d\n{\n", i+1, num_objects );
+	fprintf( out, "# Object %d of %d\n{\n", i+1, num_objects );
 
 	if ( ot == POLYGONS )
 	    polygons_to_off( &((object_list[i])->specific.polygons) );
 	else
 	    fprintf( stderr, "*** Unable to handle object type %d\n", ot );
 
-	printf( "}\n\n" );
+	fprintf( out, "}\n\n" );
     }
 }
   
@@ -80,20 +83,28 @@ void process_file( char* filename )
 
 void usage( char* argv0 )
 {
-    fprintf( stderr, "usage: %s file[.obj]\n", argv0 );
+    fprintf( stderr, "usage: %s input [output]\n", argv0 );
     fprintf( stderr, "\tOutput an OOGL file.\n" );
 }
 
 
 int main( int ac, char** av ) 
 {
-    if ( ac != 2 ) {
+    out = stdout;
+
+    if ( ac == 3 ) {
+	out = fopen(av[2],"w");
+	if ( out == NULL ) {
+	    fprintf( stderr, "Cannot open \"%s\" for output.\n", av[2] );
+	    return 1;
+	}
+    } else if ( ac != 2 ) {
 	usage( av[0] );
 	return 1;
     }
 
-    printf( "## File '%s' converted to OOGL format\n", av[1] );
-    printf( "## by bicobj2oogl $Id: bicobj2oogl.c,v 1.1 2001-02-12 18:09:48 stever Exp $\n\n" );
+    fprintf( out, "## File '%s' converted to OOGL format\n", av[1] );
+    fprintf( out, "## by bicobj2oogl $Id: bicobj2oogl.c,v 1.2 2001-06-06 01:49:58 stever Exp $\n\n" );
 
     process_file( av[1] );
 
