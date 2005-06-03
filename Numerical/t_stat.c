fa63ef3b35b440fc5b35d26a73e3aba248e63aa7
bert <bert>
2005-06-03 18:09:44 +0000

    Add local gamma() function if no public one is found in the libraries

--- Numerical/t_stat.c
+++ Numerical/t_stat.c
@@ -1,9 +1,55 @@
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include  <volume_io/internal_volume_io.h>
 #include  <bicpl.h>
 
 #define   N_POINTS         10000
 #define   MAX_DIST         100.0
 #define   N_STEPS_BETWEEN  10
+
+#if !HAVE_GAMMA
+/* log gamma function for systems that do not define it */
+double gamma(double x)
+{
+    double x0,x2,xp,gl,gl0;
+    int n,k;
+    static double a[] = {
+        8.333333333333333e-02,
+       -2.777777777777778e-03,
+        7.936507936507937e-04,
+       -5.952380952380952e-04,
+        8.417508417508418e-04,
+       -1.917526917526918e-03,
+        6.410256410256410e-03,
+       -2.955065359477124e-02,
+        1.796443723688307e-01,
+       -1.39243221690590};
+    
+    x0 = x;
+    if (x <= 0.0) return 1e308;
+    else if ((x == 1.0) || (x == 2.0)) return 0.0;
+    else if (x <= 7.0) {
+        n = (int)(7-x);
+        x0 = x+n;
+    }
+    x2 = 1.0/(x0*x0);
+    xp = 2.0*M_PI;
+    gl0 = a[9];
+    for (k=8;k>=0;k--) {
+        gl0 = gl0*x2 + a[k];
+    }
+    gl = gl0/x0+0.5*log(xp)+(x0-0.5)*log(x0)-x0;
+    if (x <= 7.0) {
+        for (k=1;k<=n;k++) {
+            gl -= log(x0-1.0);
+            x0 -= 1.0;
+        }
+    }
+    return gl;
+}
+#endif
 
 private  Real  evaluate_probability_t(
     int    v,
